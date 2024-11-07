de55c625545ddfcd7fe47fee6e9dc54ea48536c6
samithreddy39 <131654641+samithreddy39@users.noreply.github.com>
2024-11-07 19:06:54 +0530

    Update json2html.js

--- json2html.js
+++ json2html.js
@@ -1,1 +1,26 @@
 
+// json2html.js
+
+export default function json2html(data) {
+  // Start table with a data-user attribute
+  let html = '<table data-user="samithreddy888@gmail.com">';
+
+  // Table header
+  html += "<thead><tr><th>Name</th><th>Age</th><th>Gender</th></tr></thead>";
+
+  // Table body
+  html += "<tbody>";
+  data.forEach((row) => {
+    html += "<tr>";
+    html += `<td>${row.Name || ""}</td>`;
+    html += `<td>${row.Age || ""}</td>`;
+    html += `<td>${row.Gender || ""}</td>`;
+    html += "</tr>";
+  });
+  html += "</tbody>";
+
+  // Close table
+  html += "</table>";
+
+  return html;
+}
